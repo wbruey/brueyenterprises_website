8d5ce71f855b200f293b6209ba79eb2b826fc9a8
wbruey <willbruey@gmail.com>
2018-05-19 20:26:51 -0400

    rise database data and structure
    this can be loaded into mysql to create brueyent_rise database.

---
+++ newest_rise_dump.sql
@@ -0,0 +1,124 @@
+-- MySQL dump 10.13  Distrib 5.7.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: brueyent_rise
+-- ------------------------------------------------------
+-- Server version	5.7.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `brueyent_rise`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `brueyent_rise` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `brueyent_rise`;
+
+--
+-- Table structure for table `athlete`
+--
+
+DROP TABLE IF EXISTS `athlete`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `athlete` (
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  `mentor_first_name` varchar(45) DEFAULT NULL,
+  `mentor_last_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`first_name`,`last_name`),
+  KEY `athelete_fk_1_idx` (`mentor_first_name`,`mentor_last_name`),
+  CONSTRAINT `athelete_fk_1` FOREIGN KEY (`mentor_first_name`, `mentor_last_name`) REFERENCES `mentor` (`first_name`, `last_name`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `athlete`
+--
+
+LOCK TABLES `athlete` WRITE;
+/*!40000 ALTER TABLE `athlete` DISABLE KEYS */;
+INSERT INTO `athlete` VALUES ('and','another',NULL,NULL,NULL),('and','another one',NULL,NULL,NULL),('asdf','asdf',NULL,NULL,NULL),('billy','bob thorton',NULL,NULL,NULL),('Doctor','Brown',NULL,NULL,NULL),('Festy','McBob',NULL,NULL,NULL),('Horray','Jimbo',NULL,NULL,NULL),('JimGour','Bob',NULL,NULL,NULL),('Jimmy','Jimbaroo',NULL,NULL,NULL),('JimThree','Bob',NULL,NULL,NULL),('JimTwo','Bob',NULL,NULL,NULL),('MISTER','BRUESTER',NULL,NULL,NULL),('MORE','ATHELETES BRO',NULL,NULL,NULL),('more','names',NULL,NULL,NULL),('omg','didItWork',NULL,NULL,NULL),('Timmy','Smith',18,'Rebecca','Soni'),('Yolanda','Jenkins',NULL,NULL,NULL),('zeeebra','dandy',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `athlete` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `mentor`
+--
+
+DROP TABLE IF EXISTS `mentor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `mentor` (
+  `first_name` varchar(45) NOT NULL,
+  `last_name` varchar(45) NOT NULL,
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`first_name`,`last_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `mentor`
+--
+
+LOCK TABLES `mentor` WRITE;
+/*!40000 ALTER TABLE `mentor` DISABLE KEYS */;
+INSERT INTO `mentor` VALUES ('biddidy','babbdidy',23),('Caroline','Burkle',31),('Elane','Bennet',28),('Jerry','Seinfeld',NULL),('Rebecca','Soni',30),('Will','Bruey',28);
+/*!40000 ALTER TABLE `mentor` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `person`
+--
+
+DROP TABLE IF EXISTS `person`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `person` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `address_1` varchar(45) DEFAULT NULL,
+  `address_2` varchar(45) DEFAULT NULL,
+  `address_city` varchar(45) DEFAULT NULL,
+  `address_state` varchar(45) DEFAULT NULL,
+  `address_zip` varchar(45) DEFAULT NULL,
+  `address_country` varchar(45) DEFAULT NULL,
+  `phone_1` varchar(45) DEFAULT NULL,
+  `phone_2` varchar(45) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `person`
+--
+
+LOCK TABLES `person` WRITE;
+/*!40000 ALTER TABLE `person` DISABLE KEYS */;
+/*!40000 ALTER TABLE `person` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-05-19 16:25:10
