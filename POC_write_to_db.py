8bbb0d45574c1cc2782692feb59b92c34f30be8e
wbruey <willbruey@gmail.com>
2018-07-15 19:26:58 -0400

    now it modifies the database without writing over everything

--- POC_write_to_db.py
+++ POC_write_to_db.py
@@ -10,12 +10,40 @@ default_app = firebase_admin.initialize_app(cred, {'databaseURL':'https://brutes
 print (default_app.name)
 
 ref=db.reference('/')
-ref.set(
-{
-  "text" : "hey collin happy days"
-}
-)
 
+#This stuff below creaes a json tree in the database if it doesn't already exist
+"""
+ref.set({
+        'boxes': 
+            {
+                'box001': {
+                    'color': 'red',
+                    'width': 1,
+                    'height': 3,
+                    'length': 2
+                },
+                'box002': {
+                    'color': 'green',
+                    'width': 1,
+                    'height': 2,
+                    'length': 3
+                },
+                'box003': {
+                    'color': 'yellow',
+                    'width': 3,
+                    'height': 2,
+                    'length': 1
+                }
+            }
+        }
+)
+"""
+
+ref = db.reference('boxes')
+box_ref = ref.child('box001')
+box_ref.update({
+    'color': 'BRUESTER'
+})
 
 print("Script Complete")
 
