0db257be4d7c2e039879e739b929453185b69c80
Will Bruey <willbruey@gmail.com>
2018-11-25 20:25:32 -0700

    made a new website home page and now link to the rise sql database proof of concept - getting ready to create another link for the rocket simulatro

--- index.php
+++ index.php
@@ -1,49 +1,10 @@
 <html>
-	<title>RISE DATABASE 1.0</title>
+	<title>Bruey Enterprises</title>
 	<head>
-		<h1>-------</h1>
-		<h1 id="bigOne"></h1>
-		<h1>-------</h1>
-		
-		<script src="https://www.gstatic.com/firebasejs/5.2.0/firebase.js"></script>
-		<script>
-		  // Initialize Firebase
-		  var config = {
-			apiKey: "AIzaSyB1Nxa4A1ZISBjMgtTdHczNxrnCF7HZP0s",
-			authDomain: "brutest-3228a.firebaseapp.com",
-			databaseURL: "https://brutest-3228a.firebaseio.com",
-			projectId: "brutest-3228a",
-			storageBucket: "brutest-3228a.appspot.com",
-			messagingSenderId: "1099500974553"
-		  };
-		  firebase.initializeApp(config);
-		  
-		  var bigOne = document.getElementById('bigOne');
-		  var dbRef1 = firebase.database().ref().child('box001/color');
-		  var dbRef2 = firebase.database().ref().child('box001');
-		  dbRef1.on('value', snap => bigOne.innerText=snap.val());
-		  dbRef2.update({'color':'Bruester6'})
-		  
-
-		  
-		</script>
-		
-		
-		<img src="RiseLogo.jpg" width = 100% />  <!all info is in the img tag "self closing" "/" tag >
+		<h1>Bruey Enterprises</h1>
 	<head>
 	
 	<body>
-		
-		
-		
-		<h1>Forms_BRO6</h1>
-			<a href="create_athlete.php">Create Athlete</a> <br>
-			<a href="create_mentor.php">Create Mentor</a> <br>
-			<a href="create_person.php">Create Person</a>
-		
-		<h1>Reports</h1>
-			<a href="show_table.php/?table_name=athlete">All Athletes</a> <br>
-			<a href="show_table.php/?table_name=mentor">All Mentors</a> <br>
-			<a href="show_table.php/?table_name=person">All Persons</a>
+			<a href="rise_sql.php">Rise SQL Database Proof of Concept</a> <br>
 	</body>
 </html>
---
+++ rise_sql.php
@@ -0,0 +1,50 @@
+<html>
+	<title>RISE DATABASE 1.0</title>
+	<head>
+	
+<!--
+	
+		<h1 id="bigOne"></h1>
+		<h></h1>
+		
+		<script src="https://www.gstatic.com/firebasejs/5.2.0/firebase.js"></script>
+		<script>
+		  // Initialize Firebase
+		  var config = {
+			apiKey: "AIzaSyB1Nxa4A1ZISBjMgtTdHczNxrnCF7HZP0s",
+			authDomain: "brutest-3228a.firebaseapp.com",
+			databaseURL: "https://brutest-3228a.firebaseio.com",
+			projectId: "brutest-3228a",
+			storageBucket: "brutest-3228a.appspot.com",
+			messagingSenderId: "1099500974553"
+		  };
+		  firebase.initializeApp(config);
+		  
+		  var bigOne = document.getElementById('bigOne');
+		  var dbRef1 = firebase.database().ref().child('box001/color');
+		  var dbRef2 = firebase.database().ref().child('box001');
+		  dbRef1.on('value', snap => bigOne.innerText=snap.val());
+		  dbRef2.update({'color':'Bruester6'})
+-		  
+
+		  
+		</script>
+-->		
+		<img src="RiseLogo.jpg" width = 100% />  <!all info is in the img tag "self closing" "/" tag >
+	<head>
+	
+	<body>
+		
+		
+		
+		<h1>Forms</h1>
+			<a href="create_athlete.php">Create Athlete</a> <br>
+			<a href="create_mentor.php">Create Mentor</a> <br>
+			<a href="create_person.php">Create Person</a>
+		
+		<h1>Reports</h1>
+			<a href="show_table.php/?table_name=athlete">All Athletes</a> <br>
+			<a href="show_table.php/?table_name=mentor">All Mentors</a> <br>
+			<a href="show_table.php/?table_name=person">All Persons</a>
+	</body>
+</html>
